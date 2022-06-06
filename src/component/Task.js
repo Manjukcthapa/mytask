58670753c4da88bd719ac923d1ea28919552911a
Manju KC <kcmanjuthapa@gmail.com>
2022-06-06 13:12:26 -0400

    added delete icons in task component

--- src/component/Task.js
+++ src/component/Task.js
@@ -1,11 +1,12 @@
+import {FaTimes} from 'react-icons/fa'
 function Task ({task}){
 
     return(
         <>
-        <h3>{task.text}</h3>
+        <h3>{task.text} <FaTimes/></h3>
     <p>{task.date}</p>
         </>
     )
 }
 
-export default Task
+export default Task;
